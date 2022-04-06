cd53d5291d4a7556090d3b93c266436a294d20c4
elkadye <73303777+elkadye@users.noreply.github.com>
2022-04-06 02:41:03 +0200

    menu item adjustment

--- src/components/menu.product.card.js
+++ src/components/menu.product.card.js
@@ -1,11 +1,12 @@
 import * as React from "react";
-import { Grid, Typography,TextField,Card } from "@mui/material";
+import { Grid, Typography,TextField,Card,CardActions,IconButton } from "@mui/material";
 import Image from "mui-image";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 
 export default function MenuProductCard({product}) {  
        console.log(product)
   return (
-     
     <Card>
       <Grid container spacing={2}>
         <Grid item xs={6}>
@@ -13,27 +14,66 @@ export default function MenuProductCard({product}) {
         </Grid>
         <Grid item xs={6} container direction="column">
           <Grid Item>
-            <Typography variant="h5"> {product.Title}</Typography>
+            <Typography variant="h6"> {product.Title}</Typography>
           </Grid>
           <Grid Item>
-            <Typography variant="h6">Shrimp, Squid, Pineapple</Typography>
+            <Typography>Shrimp, Squid, Pineapple</Typography>
           </Grid>
           <Grid Item>
             <Typography>Price: LE. 100.00</Typography>
           </Grid>
-          <Grid Item container direction="row" alignItems="center">
-            <Grid item xs={3}>
-              +
-            </Grid>
-            <Grid item xs={6}>
-              <TextField id="outlined-basic" variant="outlined" />
-            </Grid>
-            <Grid item xs={3}>
-              -
-            </Grid>
-          </Grid>
         </Grid>
       </Grid>
+      <CardActions>
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid
+            item
+            container
+            justifyContent="flex-end"
+            alignItems="center"
+            xs={4}
+          >
+            <IconButton
+              color="primary"
+              aria-label="upload picture"
+              component="span"
+            >
+              <AddIcon />
+            </IconButton>
+          </Grid>
+          <Grid
+            item
+            xs={3}
+            container
+            justifyContent="center"
+            alignItems="center"
+          >
+            <TextField id="outlined-basic" variant="outlined" />
+            
+          </Grid>
+          <Grid
+            item
+            container
+            justifyContent="flex-start"
+            alignItems="center"
+            xs={4}
+          >
+            <IconButton
+              color="primary"
+              aria-label="upload picture"
+              component="span"
+            >
+              <RemoveIcon />
+            </IconButton>
+          </Grid>
+
+        </Grid>
+      </CardActions>
     </Card>
   );
 }
