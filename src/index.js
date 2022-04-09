c822e162b50d04f7530a5958e08ad74217283fa8
elkadye <73303777+elkadye@users.noreply.github.com>
2022-04-10 01:03:49 +0200

    starting typeorm

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
+import "reflect-metadata"
 import App from "./App";
 
 import { BrowserRouter } from "react-router-dom";
@@ -18,16 +18,6 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
-// const store = createStore(
-//   reducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-// );
-
-// const store = createStore(
-//   reducers,
-//   compose(applyMiddleware(thunk)),
-//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-// );
-
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -39,7 +29,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+9mX8!.r2N48ZE!z
